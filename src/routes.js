97d102c10d1427e6f1ea9a1f9f1f55795c7447d5
Murilo <murilinhops@hotmail.com>
2020-03-26 14:17:28 -0300

    initializing project

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const AppStack = createStackNavigator();
+
+
+
+import Cases from './pages/cases';
+import Details from './pages/details';
+
+export default function Routes() {
+    
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{headerShown: false}}>
+          <AppStack.Screen name="Cases" component={Cases}></AppStack.Screen>
+          <AppStack.Screen name="Details" component={Details}></AppStack.Screen>
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+}
